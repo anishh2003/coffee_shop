bb934d7316efe6866b4474763313e5a93e900b31
Anish <anishh2003@gmail.com>
2024-02-13 10:36:54 +0000

    dark theme applied

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFFC88850));
+var kColorLightScheme =
+    ColorScheme.fromSeed(seedColor: const Color(0xFFC88850));
+var kColorDarkScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF042A49));
--- lib/main.dart
+++ lib/main.dart
@@ -18,66 +18,67 @@ class MainApp extends ConsumerWidget {
     var darkThemeToggle = ref.watch(lightThemeProvider);
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        colorScheme: kColorScheme,
+        brightness: Brightness.light,
+        colorScheme: kColorLightScheme,
         textTheme: appTheme.copyWith(
           headlineLarge: GoogleFonts.archivoNarrow(
-            color: kColorScheme.onPrimary,
+            color: kColorLightScheme.onPrimary,
             fontStyle: FontStyle.italic,
           ),
           headlineMedium: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorLightScheme.primary,
             fontStyle: FontStyle.italic,
           ),
           headlineSmall: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorLightScheme.primary,
             fontStyle: FontStyle.italic,
           ),
           titleSmall: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorLightScheme.primary,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.italic,
           ),
           titleMedium: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorLightScheme.primary,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.italic,
           ),
           titleLarge: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorLightScheme.primary,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.italic,
           ),
           bodySmall: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorLightScheme.primary,
             fontStyle: FontStyle.italic,
           ),
           bodyMedium: GoogleFonts.archivoNarrow(
-            color: kColorScheme.secondary,
+            color: kColorLightScheme.secondary,
             fontStyle: FontStyle.italic,
             fontSize: 15.0,
           ),
           bodyLarge: GoogleFonts.archivoNarrow(
-            color: kColorScheme.secondary,
+            color: kColorLightScheme.secondary,
             fontStyle: FontStyle.italic,
           ),
           labelMedium: GoogleFonts.archivoNarrow(
-              color: kColorScheme.primary,
+              color: kColorLightScheme.primary,
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.bold,
               fontSize: 15.0),
           labelSmall: GoogleFonts.archivoNarrow(
-              color: kColorScheme.primary,
+              color: kColorLightScheme.primary,
               fontWeight: FontWeight.w100,
               fontSize: 14.0),
         ),
         appBarTheme: AppBarTheme(
-          backgroundColor: kColorScheme.primary,
+          backgroundColor: kColorLightScheme.primary,
         ),
         cardTheme: CardTheme(
-            surfaceTintColor: kColorScheme.surfaceTint, elevation: 15.0),
+            surfaceTintColor: kColorLightScheme.surfaceTint, elevation: 15.0),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primary,
+            backgroundColor: kColorLightScheme.primary,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(5), // <-- Radius
             ),
@@ -86,74 +87,75 @@ class MainApp extends ConsumerWidget {
         scrollbarTheme: ScrollbarThemeData(
           thumbColor: MaterialStateColor.resolveWith((states) {
             if (states.contains(MaterialState.pressed)) {
-              return kColorScheme.primary
+              return kColorLightScheme.primary
                   .withOpacity(0.8); // Adjust opacity or color as needed
             }
-            return kColorScheme.primary; // Default color when not pressed
+            return kColorLightScheme.primary; // Default color when not pressed
           }),
         ),
       ),
       darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kColorScheme,
+        brightness: Brightness.dark,
+        colorScheme: kColorDarkScheme,
         textTheme: appTheme.copyWith(
           headlineLarge: GoogleFonts.archivoNarrow(
-            color: kColorScheme.onPrimary,
+            color: kColorDarkScheme.onPrimary,
             fontStyle: FontStyle.italic,
           ),
           headlineMedium: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorDarkScheme.primary,
             fontStyle: FontStyle.italic,
           ),
           headlineSmall: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorDarkScheme.primary,
             fontStyle: FontStyle.italic,
           ),
           titleSmall: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorDarkScheme.primary,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.italic,
           ),
           titleMedium: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorDarkScheme.primary,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.italic,
           ),
           titleLarge: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorDarkScheme.primary,
             fontWeight: FontWeight.w600,
             fontStyle: FontStyle.italic,
           ),
           bodySmall: GoogleFonts.archivoNarrow(
-            color: kColorScheme.primary,
+            color: kColorDarkScheme.primary,
             fontStyle: FontStyle.italic,
           ),
           bodyMedium: GoogleFonts.archivoNarrow(
-            color: kColorScheme.secondary,
+            color: kColorDarkScheme.secondary,
             fontStyle: FontStyle.italic,
             fontSize: 15.0,
           ),
           bodyLarge: GoogleFonts.archivoNarrow(
-            color: kColorScheme.secondary,
+            color: kColorDarkScheme.secondary,
             fontStyle: FontStyle.italic,
           ),
           labelMedium: GoogleFonts.archivoNarrow(
-              color: kColorScheme.primary,
+              color: kColorDarkScheme.primary,
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.bold,
               fontSize: 15.0),
           labelSmall: GoogleFonts.archivoNarrow(
-              color: kColorScheme.primary,
+              color: kColorDarkScheme.primary,
               fontWeight: FontWeight.w100,
               fontSize: 14.0),
         ),
         appBarTheme: AppBarTheme(
-          backgroundColor: kColorScheme.primary,
+          backgroundColor: kColorDarkScheme.primary,
         ),
         cardTheme: CardTheme(
-            surfaceTintColor: kColorScheme.surfaceTint, elevation: 15.0),
+            surfaceTintColor: kColorDarkScheme.surfaceTint, elevation: 15.0),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primary,
+            backgroundColor: kColorDarkScheme.primary,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(5), // <-- Radius
             ),
@@ -162,10 +164,10 @@ class MainApp extends ConsumerWidget {
         scrollbarTheme: ScrollbarThemeData(
           thumbColor: MaterialStateColor.resolveWith((states) {
             if (states.contains(MaterialState.pressed)) {
-              return kColorScheme.primary
+              return kColorDarkScheme.primary
                   .withOpacity(0.8); // Adjust opacity or color as needed
             }
-            return kColorScheme.primary; // Default color when not pressed
+            return kColorDarkScheme.primary; // Default color when not pressed
           }),
         ),
       ),
